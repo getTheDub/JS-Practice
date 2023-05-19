dd87ba2e14b206aeebcea72e4b8887663e6afbb6
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-05-19 13:57:18 -0400

    Debug session: array methods

--- debugging-practice.js
+++ debugging-practice.js
@@ -1,7 +1,20 @@
-function filterRangeInPlace(arr, a, b) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < a || arr[i] > b) {
-      arr.splice(i, 1);
-    }
-  }
+let users = [
+	{id: 'john', name: "John Smith", age: 20},
+	{id: 'ann', name: "Ann Smith", age: 24},
+	{id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+console.log(usersById);
+
+function groupById(array) {
+	return array.reduce((obj, item) => {
+		obj[item.id] = item;
+		return obj
+	}, {})
 }
+
+// console.log(users[0].id);
+
+
+debugger;
